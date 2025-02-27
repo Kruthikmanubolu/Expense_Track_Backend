f87f1131c968e0064f391757711e884ef12dccf1
“Kruthikmanubolu” <kruthikmanubolu@gmail.com>
2025-02-26 18:28:27 -0800

    IDK

--- server.js
+++ server.js
@@ -11,9 +11,11 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
+  
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/expenses', require('./routes/expenses'));
+app.use('/api/incomes', require('./routes/incomes'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,'0.0.0.0', () => console.log(`Server running on port ${PORT}`));
 
